8d8b6918eb90c14cb3288403116a78306a22deaa
Daniele Sergio <daniele.sergio0@gmail.com>
2025-11-12 13:05:31 +0100

    Add default null value to PostImp constructor for id

--- app/src/main/java/com/danielesergio/zextrastest/model/post/PostImp.kt
+++ app/src/main/java/com/danielesergio/zextrastest/model/post/PostImp.kt
@@ -3,4 +3,4 @@ package com.danielesergio.zextrastest.model.post
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PostImp(override val id:Long?, override val userId:Long, override val title:String, override val body:String):Post
+data class PostImp(override val id:Long? = null, override val userId:Long, override val title:String, override val body:String):Post
