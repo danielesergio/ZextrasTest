65367be25298fb94a2060bf2124bb694bb2865c6
Daniele Sergio <daniele.sergio0@gmail.com>
2025-11-12 18:49:07 +0100

    Create utility methods for converting purpose
    Create methods that converts UI State objects to Model object,
    and vice versa

---
+++ app/src/main/java/com/danielesergio/zextrastest/android/state/StateModelConverter.kt
@@ -0,0 +1,25 @@
+package com.danielesergio.zextrastest.android.state
+
+import com.danielesergio.zextrastest.R
+import com.danielesergio.zextrastest.model.ValidationResult
+import com.danielesergio.zextrastest.model.post.Post
+import com.danielesergio.zextrastest.model.post.PostImp
+
+//Model to UI state
+fun Post.toPostState(): PostState = PostState(id!!, title, body)
+fun List<Post>.toPostsState(): PostMasterDetailsState = PostMasterDetailsState(
+    posts = map { it.toPostState() },
+    isPending = false
+)
+
+//UI state to Model
+fun PostFormState.toPost(userId:Long): Post = PostImp(title = title, body = body, userId = userId)
+
+
+//Error code to Android string id
+fun ValidationResult.toStringID():Int?{
+    return when(this){
+        ValidationResult.VALID-> null
+        ValidationResult.INVALID_EMPTY_FIELD -> R.string.error_empty_field_message
+    }
+}
