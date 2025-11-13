55e0186ece7dbec7f3f1a003a3dffe485144492d
Daniele Sergio <daniele.sergio0@gmail.com>
2025-11-13 17:16:15 +0100

    Remove PostMasterDetailsState and PostMasterDetailsViewModel

--- app/src/main/java/com/danielesergio/zextrastest/android/state/PostMasterDetailsState.kt
+++
@@ -1,6 +0,0 @@
-package com.danielesergio.zextrastest.android.state
-
-data class PostMasterDetailsState(val posts:List<PostState> = emptyList(),
-                                  val selectedItem: PostState? = null,
-                                  val isPending: Boolean = true,
-                                  val syncError: Boolean = false)
