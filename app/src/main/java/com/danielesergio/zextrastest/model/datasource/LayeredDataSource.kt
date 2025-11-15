5a3498369ea9e1c7d053bd2b4109f51edbdfc694
Daniele Sergio <daniele.sergio0@gmail.com>
2025-11-15 18:35:22 +0100

    Improve implementation of LayeredDataSource.getPosts
    Return post sorted in reverse chronological order.
    Correct the merging process by putting the posts retrieved
    from the patchedDataSource before the others.

--- app/src/main/java/com/danielesergio/zextrastest/model/datasource/LayeredDataSource.kt
+++ app/src/main/java/com/danielesergio/zextrastest/model/datasource/LayeredDataSource.kt
@@ -2,7 +2,8 @@ package com.danielesergio.zextrastest.model.datasource
 
 import com.danielesergio.zextrastest.log.LoggerImpl
 import com.danielesergio.zextrastest.model.post.Post
-import kotlin.math.min
+import kotlin.math.ceil
+import kotlin.math.max
 
 /**
  * A [LayeredDataSource] is a [DataSource] which simulate a mutable datasource starting from an immutable one.
@@ -11,33 +12,34 @@ import kotlin.math.min
  *
  * @property patchedDataSource the datasource that contained the new posts.
  */
+//todo add test cases
 class LayeredDataSource(val immutableDataSource: DataSource, val patchedDataSource: DataSource): DataSource{
 
     override suspend fun getPosts(page: Int?, responseSize: Int?, after: Long?): List<Post> {
-        val posts = immutableDataSource.getPosts(page)
-        LoggerImpl.d(TAG, "Obtained post from immutableDatasource older than " +
-                "$after, page $page, page max size $responseSize, element in page ${posts.size}")
+        val posts = patchedDataSource.getPosts(page)
+        LoggerImpl.d(TAG,logMessage("patchedDataSource", page, responseSize, after) )
+        LoggerImpl.d(TAG, logTotalPost(posts.size))
 
         val mergedPosts = when{
 
-            page == null -> posts.plus(patchedDataSource.getPosts(page, responseSize,after))
+            responseSize == null -> posts.plus(immutableDataSource.getPosts(page, responseSize,after))
 
-            posts.size < PAGE_SIZE -> {
-                val patchedDataSourcePage =
-                    (page * PAGE_SIZE - TOTAL_ELEMENTS_MAIN_DATASOURCE) / PAGE_SIZE
-                val othersPosts = patchedDataSource.getPosts(patchedDataSourcePage, responseSize, after)
-                LoggerImpl.d(TAG, "Obtained post from patchedDataSource older " +
-                        "than ($after), page $patchedDataSourcePage, , page max size $responseSize " +
-                        "element in page ${othersPosts.size}")
-                posts.plus(othersPosts).run {
-                    this.subList(0, min(PAGE_SIZE, size))
-                }
+            posts.size < responseSize -> {
+
+                val othersPosts = getPostToMerge(page?:1,
+                    responseSize,
+                    after,
+                    patchedDataSource.getTotalPosts())
+                    .take(responseSize - posts.size)
+
+                posts.plus(othersPosts)
             }
 
             else -> posts
         }
-        LoggerImpl.i(TAG, "Obtained post from LayeredDataSource,  older than " +
-                "$after page $page, page max size $responseSize, element in page ${mergedPosts.size}")
+        LoggerImpl.i(TAG, logMessage("LayeredDataSource", page, responseSize, after))
+        LoggerImpl.i(TAG, logTotalPost(mergedPosts.size) )
+
         return mergedPosts
     }
 
@@ -58,11 +60,35 @@ class LayeredDataSource(val immutableDataSource: DataSource, val patchedDataSour
         return patchedDataSource.getTotalPosts() + immutableDataSource.getTotalPosts()
     }
 
-    //todo move PAGE_SIZE and TOTAL_ELEMENTS_MAIN_DATASOURCE -> update dataSource interface to return these data.
-    companion object{
-        private const val PAGE_SIZE:Int = 10
-        private const val TOTAL_ELEMENTS_MAIN_DATASOURCE:Int = 100
+    private suspend fun getPostToMerge(
+        globalPage:Int,
+        responseSize:Int,
+        after: Long? ,
+        totalElementOtherDS: Long ):List<Post>{
+        val lastElementToShow = globalPage * responseSize - totalElementOtherDS
+        if(lastElementToShow <= 0){
+            LoggerImpl.d(TAG, "No post to merge")
+            return emptyList()
+        }
+        val firstElementToShow = max(lastElementToShow - responseSize + 1, 1)
+        val firstPage = ceil(firstElementToShow.toDouble() / responseSize).toInt()
+        val secondPage = ceil(lastElementToShow.toDouble() / responseSize).toInt()
+        LoggerImpl.d(TAG, "Post to merge from immutableDataSource [$firstElementToShow, $lastElementToShow]")
+        LoggerImpl.d(TAG, "Page first element: $firstPage")
+        LoggerImpl.d(TAG, "Page last element: $secondPage")
+        val posts = immutableDataSource.getPosts(firstPage, responseSize, after)
+            .drop(firstElementToShow.toInt() % responseSize - 1)
+        return if(firstPage != secondPage) {
+            posts + immutableDataSource.getPosts(secondPage, responseSize, after)
+        } else {
+            posts
+        }
+    }
 
+    private fun logMessage(ds:String, page:Int?, responseSize:Int?, after:Long?) =
+        "Obtained post from $ds.getPosts($page, $responseSize, $after)"
+    private fun logTotalPost(totalPost:Int)= "Total posts obtained: $totalPost"
+    companion object{
         private val TAG = LayeredDataSource::class.java.simpleName
     }
 }
