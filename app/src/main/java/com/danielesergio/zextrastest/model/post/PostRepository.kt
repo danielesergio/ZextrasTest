e509538a8086dff1bd44abcffbdfe1d65377ce4b
Daniele Sergio <daniele.sergio0@gmail.com>
2025-11-16 12:45:49 +0100

    Throw IllegalArgumentException in PostRepository.add
    Throw IllegalArgumentException in PostRepository.add when the
    post argument fail validation.

--- app/src/main/java/com/danielesergio/zextrastest/model/post/PostRepository.kt
+++ app/src/main/java/com/danielesergio/zextrastest/model/post/PostRepository.kt
@@ -1,6 +1,7 @@
 package com.danielesergio.zextrastest.model.post
 
 import com.danielesergio.zextrastest.log.LoggerImpl
+import com.danielesergio.zextrastest.model.ValidationResult
 import com.danielesergio.zextrastest.model.datasource.DataSource
 
 class PostRepository(private val dataSource: DataSource){
@@ -15,6 +16,9 @@ class PostRepository(private val dataSource: DataSource){
 
     suspend fun add(post: Post): Result<Post>{
         return runCatching {
+            if(PostValidator.validateTitle(post.title) == ValidationResult.INVALID_EMPTY_FIELD){
+                throw IllegalArgumentException("Title can't be empty")
+            }
             dataSource.createPost(post)
         }.onFailure {
             LoggerImpl.w(TAG, "Exception adding $post", it)
